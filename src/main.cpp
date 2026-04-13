7510eaf8b179b7e7293199d0047e8b14e4243c01
testdriveupgrade <totveto@gmail.com>
2026-04-13 16:12:36 +0300

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+
+// testrec - ReXGlue Recompiled Project
+//
+// This file is yours to edit. 'rexglue migrate' will NOT overwrite it.
+
+#include "generated/testrec_config.h"
+#include "generated/testrec_init.h"
+
+#include "testrec_app.h"
+
+REX_DEFINE_APP(testrec, TestrecApp::Create)
